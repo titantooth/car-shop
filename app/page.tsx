cdc4cebd8351b1358c76397c107031c1b587beab
titantooth <ali.rabieeie@gmail.com>
2023-08-21 12:58:30 +0330

    car purchase catalog

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Hero } from '@/components'
+import { CustomFilter, Hero, SearchBar } from '@/components'
 import Image from 'next/image'
 
 
@@ -6,6 +6,22 @@ export default function Home() {
   return (
     <main className="overflow-hidden ">
       <Hero />
+      <div className='mt-12 padding-x padding-y max-width' id="discover">
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+          <p>Explore the Cars You Might Like</p>
+
+        </div>
+        <div className='home__filters'>
+          <SearchBar />
+          <div className='home__filter-container'>
+            <CustomFilter title='fuel' />
+            <CustomFilter title='year' />
+          </div>
+
+        </div>
+
+      </div>
     </main>
   )
 }
