045de0813e2f022d14b19848ba994d77599712b0
titantooth <ali.rabieeie@gmail.com>
2023-08-22 21:34:51 +0330

    carCard added pictures and rent

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import Image from 'next/image'
 import { CarProps } from '@/types'
 import CustomButton from './CustomButton'
+import { calculateCarRent } from '@/utils'
 
 
 interface CarCardProps {
@@ -11,6 +12,7 @@ interface CarCardProps {
 
 const CarCard = ({ car }: CarCardProps) => {
     const { city_mpg, year, make, model, transmission, drive } = car;
+    const carRent = calculateCarRent(city_mpg, year)
     return (
         <div className='car-card group:'>
             <div className='car-card__content'>
@@ -18,11 +20,18 @@ const CarCard = ({ car }: CarCardProps) => {
                     {make} {model}
                 </h2>
             </div>
-            <p>
-                <span>
-                    Car Rent...
+            <p className='flex mt-6 text-[32px] font-extrabold'>
+                <span className='self-start text-[14px] font-semibold'>
+                    $
+                </span>
+                {carRent}
+                <span className='self-end text-[14px] font-medium'>
+                    /day
                 </span>
             </p>
+            <div className='realtive w-full h-40 my-3 object-contain'>
+                <Image src='/hero.png' alt='car modek' fill priority className='object-contain' />
+            </div>
         </div>
     )
 }
