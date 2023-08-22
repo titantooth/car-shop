3c87a7c205a15dec91892a69ab2e8e19c77233a7
titantooth <ali.rabieeie@gmail.com>
2023-08-22 21:20:43 +0330

    worked a bit on carCard and API

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+'use client'
+import { useState } from 'react'
+import Image from 'next/image'
+import { CarProps } from '@/types'
+import CustomButton from './CustomButton'
 
-const CarCard = (card) => {
+
+interface CarCardProps {
+    car: CarProps
+}
+
+const CarCard = ({ car }: CarCardProps) => {
+    const { city_mpg, year, make, model, transmission, drive } = car;
     return (
-        <div>CarCard</div>
+        <div className='car-card group:'>
+            <div className='car-card__content'>
+                <h2 className='car-card__content-title'>
+                    {make} {model}
+                </h2>
+            </div>
+            <p>
+                <span>
+                    Car Rent...
+                </span>
+            </p>
+        </div>
     )
 }
 
