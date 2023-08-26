4a1826b4edc57f97aa61a6cf209039dd51f32430
titantooth <ali.rabieeie@gmail.com>
2023-08-26 12:48:04 +0330

    api bugged

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import Image from 'next/image'
 import { Fragment } from 'react'
+import { generateCarImageUrl } from '@/utils'
 
 interface CarDetailsProps {
     isOpen: boolean
@@ -42,22 +43,22 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
 
                                     <div className='flex-1 flex flex-col gap-3 '>
                                         <div className='relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg'>
-                                            <Image src='/hero.png' alt='car modek' fill priority className='object-contain' />
+                                            <Image src={generateCarImageUrl(car)} alt='car modek' fill priority className='object-contain' />
 
                                         </div>
                                         <div className='flex gap-3'>
                                             <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
-                                                <Image src='/hero.png' alt='car modek' fill priority className='object-contain' />
+                                                <Image src={generateCarImageUrl(car, '29')} alt='car modek' fill priority className='object-contain' />
 
 
                                             </div>
                                             <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
-                                                <Image src='/hero.png' alt='car modek' fill priority className='object-contain' />
+                                                <Image src={generateCarImageUrl(car, '33')} alt='car modek' fill priority className='object-contain' />
 
 
                                             </div>
                                             <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
-                                                <Image src='/hero.png' alt='car modek' fill priority className='object-contain' />
+                                                <Image src={generateCarImageUrl(car, '13')} alt='car modek' fill priority className='object-contain' />
 
 
                                             </div>
