267d9b4eed4e645e06ed523beb6709a9dc2b7fa8
titantooth <ali.rabieeie@gmail.com>
2023-08-26 11:29:07 +0330

    styled

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -22,7 +22,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                     <div className='fixed inset-0 overflow-y-auto'>
                         <div className='flex min-h-full items-center justify-center p-4 text-center'>
                             <Transition.Child as={Fragment} enter='ease-out duration-300' enterFrom='opacity-0 scale-95' enterTo='opacity-100 scale-100' leave='ease-in duration-200' leaveFrom='opacity-100 scale-100' leaveTo='opacity-0 scale-95'>
-                                <Dialog.Panel className='relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xl transition-all flex flex-col gap-5'>
+                                <Dialog.Panel className='relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col gap-5'>
                                     <button
                                         type='button'
                                         onClick={closeModal}
@@ -69,7 +69,14 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                                             {car.make} {car.model}
                                         </h2>
                                         <div className='mt-3 flex flex-wrap gap-4 '>
-
+                                            {Object.entries(car).map(([key, value]) => (
+                                                <div className='flex justify-between gap-5 w-full text-right' key={key}>
+                                                    <h4 className='text-grey capitalize'>
+                                                        {key.split('_').join(' ')}
+                                                    </h4>
+                                                    <p className='text-black-100 font-semibold'>{value}</p>
+                                                </div>
+                                            ))}
                                         </div>
                                     </div>
                                 </Dialog.Panel>
