f49dc837e3f5a7957597605544dd488439e1cb80
titantooth <ali.rabieeie@gmail.com>
2023-08-27 21:09:16 +0330

    custom filter function

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { CustomFilterProps } from '@/types'
+import { updateSearchParams } from '@/utils'
 import { Listbox, Transition } from '@headlessui/react'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
@@ -8,12 +9,27 @@ Listbox
 
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
+    const router = useRouter()
+
     const [selected, setSelected] = useState(options[0])
+
+    const handleUpdateParams = (e: { title: string, value: string }) => {
+        const newPathName = updateSearchParams(title, e.value.toLowerCase())
+
+
+        router.push(newPathName)
+    }
+
+
+
     return (
         <div className='w-fit'>
             <Listbox
                 value={selected}
-                onChange={(e) => setSelected(e)}
+                onChange={(e) => {
+                    setSelected(e);
+                    handleUpdateParams(e)
+                }}
             >
                 <div className='relative w-fit z-10'>
                     <Listbox.Button className='custom-filter__btn'>
--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,6 @@
 import { CarProps, FilterProps } from "@/types";
+import exp from "constants";
+import { type } from "os";
 
 export async function fetchCars(filters: FilterProps) {
   const { manufacturer, year, model, limit, fuel } = filters;
@@ -49,4 +51,14 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   url.searchParams.append("angle", `${angle}`);
 
   return `${url}`;
+};
+
+export const updateSearchParams = (type: string, value: string) => {
+  const searchParams = new URLSearchParams(window.location.search);
+
+  searchParams.set(type, value);
+
+  const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+
+  return newPathname;
 };
