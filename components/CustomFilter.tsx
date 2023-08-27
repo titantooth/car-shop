8cdb36ae7c67f8174218e3aedf1acc7c4fbb4cfd
titantooth <ali.rabieeie@gmail.com>
2023-08-27 19:51:51 +0330

    start custom filter

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,4 +1,10 @@
-import React from 'react'
+"use client"
+import { Listbox } from '@headlessui/react'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import React, { Fragment, useState } from 'react'
+Listbox
+
 
 const CustomFilter = () => {
     return (
