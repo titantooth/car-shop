1061800b29a971ce7ff006de9cd5a4609699028b
titantooth <ali.rabieeie@gmail.com>
2023-08-27 20:35:36 +0330

    custom filter and its props and their TS types

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,12 +1,13 @@
 "use client"
-import { Listbox } from '@headlessui/react'
+import { CustomFilterProps } from '@/types'
+import { Listbox, Transition } from '@headlessui/react'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import React, { Fragment, useState } from 'react'
 Listbox
 
 
-const CustomFilter = () => {
+const CustomFilter = ({ title, options }: CustomFilterProps) => {
     return (
         <div>CustomFilter</div>
     )
--- types/index.ts
+++ types/index.ts
@@ -36,3 +36,13 @@ export interface FilterProps {
   limit: number;
   model: string;
 }
+
+export interface OptionProps {
+  title: string;
+  value: string;
+}
+
+export interface CustomFilterProps {
+  title: string;
+  options: OptionProps[];
+}
